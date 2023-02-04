422647908dc8d8d538cc90c39791bd667f039388
Rohit <rohitp934@gmail.com>
2023-02-04 14:11:35 +0530

    Initialized rust project with lib

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// Library to spawn process in parallel and execute
+// source code in various languages.
+// Author: @rohitp934
+// License: MIT
+// Version: 0.1.0
+
+pub fn code_checker() {
+    println!("Hello, world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rustycoding::code_checker;
+
+fn main() {
+    // Call the code_checker function in lib.rs
+    code_checker();
+}
