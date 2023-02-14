3146866d4a0c9ef19cd78679463a27c6420b93be
Rohit <rohitp934@gmail.com>
2023-02-14 19:04:23 +0530

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use actix_web::{get, Responder, HttpResponse, HttpServer, App};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
+use serde::Deserialize;
 // use rustycoding::code_checker;
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Actix Web Rust Server is running!")
 }
+#[derive(Deserialize)]
+struct CheckQuery {
+    value: bool,
+}
+
+#[get("/check")]
+async fn check(info: web::Query<CheckQuery>) -> impl Responder {
+    if info.value {
+        HttpResponse::Ok().body("Everything looks good!")
+    } else {
+        HttpResponse::BadRequest().body("You messed up fr!")
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
-        App::new()
-            .service(index)
+        let logger = Logger::default();
+        App::new().wrap(logger).service(index).service(check)
     })
     .bind(("127.0.0.1", 3000))?
     .run()
