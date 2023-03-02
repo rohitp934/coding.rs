9631cbc90c16f914317598208486c21aacf32cc5
Rohit <rohitp934@gmail.com>
2023-03-02 11:56:11 +0530

    Switched to ENV PORT

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ use routes::config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .unwrap();
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         let logger = Logger::new("\"%r\" %s (%b bytes) %Dms");
         App::new().wrap(logger).configure(config)
     })
-    .bind(("127.0.0.1", 3000))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
